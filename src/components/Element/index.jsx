b589abc3d058d6b2ece72c621efc2c6e308bdf8f
ryaovan <ryaovan@gmail.com>
2020-08-17 19:02:24 -0700

    add handler for checkbox

--- src/components/Element/index.jsx
+++ src/components/Element/index.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
-const Element = ({ roomName, count }) => (
-  <div className="element-item">
-    <input type="checkbox" id={`${roomName}-checkbox`} />
-    <label htmlFor={`${roomName}-checkbox`}>{`${roomName} (${count})`}</label>
-  </div>
-);
+class Element extends Component {
+  constructor(props) {
+    super(props);
+    const { roomName, count } = props;
+    this.state = {
+      checked: true,
+    };
+    this.roomName = roomName;
+    this.count = count;
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+
+  handleCheck() {
+    this.setState({
+      checked: !this.state.checked,
+    });
+  }
+
+  render() {
+    return (
+      <div className="element-item">
+        <input
+          type="checkbox"
+          id={`${this.roomName}-checkbox`}
+          checked={this.state.checked}
+          onClick={this.handleCheck}
+        />
+        <label htmlFor={`${this.roomName}-checkbox`}>{`${this.roomName} (${this.count})`}</label>
+      </div>
+    );
+  }
+}
 
 export default Element;
